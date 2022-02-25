8cf784213c09c2d6fa500bc3117de604024e696d
sukhvinderkaur26 <sukhvinder2630@gmail.com>
2022-02-24 19:54:49 -0700

    Vhanged code

--- src/Assignment7.java
+++ src/Assignment7.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Assignment7 {
     public static void main(String[] args) {
+        System.out.println("Its changing code");
         //Declare an array of char, and store your first name
         char firstname[] = {'S', 'u', 'k', 'h', 'v', 'i', 'n', 'd', 'e', 'r'};
         System.out.print("First name is : ");
